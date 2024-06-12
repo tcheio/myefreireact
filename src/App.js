113c680549d88fb68f20de0266614e61662ac636
tcheio <thomas.cheio0105@gmail.com>
2024-06-12 11:54:02 +0200

    Détails enseignant

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Accueil from './Components/Accueil';
 import ListeEtudiants from './Components/ListeEtudiants';
-import ListeEnseignants from './Components/ListeEnseignants';
+import ListeEnseignants from './Components/Enseignants/ListeEnseignants';
+import EnseignantDetail from './Components/Enseignants/EnseignantDetail';
 import Classes from './Components/Classes';
 import './App.css';
 
@@ -15,6 +16,7 @@ function App() {
             <Route path="/" element={<Accueil />} />
             <Route path="/etudiant" element={<ListeEtudiants />} />
             <Route path="/enseignant" element={<ListeEnseignants />} />
+            <Route path="/enseignants/:id" element={<EnseignantDetail />} />
             <Route path="/classes" element={<Classes />} />
           </Routes>
           <nav>
