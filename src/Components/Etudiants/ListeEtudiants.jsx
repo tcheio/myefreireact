1c679c79149063f8823bbec20dc89e4a6c6bc2c0
tcheio <thomas.cheio0105@gmail.com>
2024-06-12 17:15:45 +0200

    reparation listeEtudiant

--- src/Components/Etudiants/ListeEtudiants.jsx
+++ src/Components/Etudiants/ListeEtudiants.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
-import etudiants from '../../Données/Etudiant';
+import etudiantsData from '../../Données/Etudiant';
 import classesIndex from '../../Données/Classes';
 import './ListeEtudiants.css';
 import { Link } from 'react-router-dom';
 
 const ListeEtudiants = () => {
   const [classeSelectionnee, setClasseSelectionnee] = useState('Toutes');
-  const [etudiants, setEtudiants] = useState(etudiants);
+  const [etudiants, setEtudiants] = useState(etudiantsData);
 
   const classes = ['Toutes', ...new Set(classesIndex.map(classe => classe.nom))];
 
@@ -45,16 +45,13 @@ const ListeEtudiants = () => {
         <div key={index} className="etudiantCard">
           <Link to={`/etudiant/${index}`}><p><strong>{etudiant.prenom} {etudiant.nom}</strong></p></Link>
           <p>Âge : {etudiant.age} ans</p>
-          <p>Classe : {etudiant.classe}</p>
+          <p>Classe : {getClasseNom(etudiant.classesId)}</p>
           <button onClick={() => supprimerEtudiant(index)}>Supprimer</button>
         </div>
       ))}
       <Link to="/add-etudiant">
         <button>Ajouter un étudiant</button>
       </Link>
-          <p>Classe : {getClasseNom(etudiant.classesId)}</p>
-        </div>
-      ))}
     </div>
   );
 };
